462477a778cc22744f93534cf433b0150662c738
pennypesh <48815910+pennypesh@users.noreply.github.com>
2021-09-01 18:17:53 -0500

    initial operator commit

---
+++ OperatorsRunner.java
@@ -0,0 +1,32 @@
+public class OperatorsRunner{
+    public static void main(String[]args){
+        System.out.println("Operators runner has started!!");
+
+
+        boolean flag = true;
+
+        int valOne =234;
+        int valTwo =22;
+        int answer = 0;
+
+        // Arithmetic operators
+        answer = valOne + valTwo;
+        System.out.println("The Sum of valOne and valTwo is "+answer);
+
+        answer = valOne - valTwo;
+        System.out.println("Value of valOne and valTwo is "+answer);
+
+        answer = valOne / valTwo;
+        System.out.println("The value of valOne and valTwo is "+answer);
+
+        answer = valOne * valTwo;
+        System.out.println("The value of valOne and valTwo is "+answer);
+
+
+
+
+    
+
+
+    }
+}
