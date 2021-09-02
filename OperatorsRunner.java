e39a8f0feecf77bd4fb200e1bcf34ac9d7e15e62
pennypesh <48815910+pennypesh@users.noreply.github.com>
2021-09-02 15:19:58 -0500

    second commit on operators

--- OperatorsRunner.java
+++ OperatorsRunner.java
@@ -7,7 +7,7 @@ public class OperatorsRunner{
 
         int valOne =234;
         int valTwo =22;
-        int answer = 0;
+        double answer = 0;
 
         // Arithmetic operators
         answer = valOne + valTwo;
@@ -16,11 +16,60 @@ public class OperatorsRunner{
         answer = valOne - valTwo;
         System.out.println("Value of valOne and valTwo is "+answer);
 
-        answer = valOne / valTwo;
+        answer = (double)valOne / valTwo;
         System.out.println("The value of valOne and valTwo is "+answer);
 
         answer = valOne * valTwo;
         System.out.println("The value of valOne and valTwo is "+answer);
+
+// modulus returns the remainder
+        answer = (double)valOne % valTwo;
+        System.out.println("The value of valOne and valTwo is "+answer);
+
+// increment
+         
+         int x = 30;
+         ++x;
+         System.out.println("pre Increment: "+x);
+        
+         System.out.println("Post Increment: "+(++x));
+
+        //  Decrement
+        int y=30;
+        --y;
+        System.out.println("pre decrement: "+y);
+        
+         System.out.println("Post decrement: "+(y--));
+
+
+        //  Comparison operators
+
+        boolean trueFlag=true;
+        // boolean isTrue =false;
+        boolean isEqual = valOne == valOne;
+        System.out.println("ValOne is equal to valOne --> "+isEqual);
+
+        boolean isNotEqual = valOne != valOne;
+        System.out.println("valOne is not equal to valOne --> "+isNotEqual);
+         
+        boolean isGreaterThan = valOne > valTwo;
+        System.out.println(valOne+"is greater than "+valTwo+"-->"+isGreaterThan);
+        
+        boolean isLessThan = valOne < valOne;
+        System.out.println(valOne+"is less than "+valTwo+"-->"+isLessThan);
+        
+
+        // Assignment Operators
+         int testVal = 30;
+         testVal += 2;
+         System.out.println("(30) += 2 -->"+testVal);
+
+
+         
+
+
+
+
 
 
 
